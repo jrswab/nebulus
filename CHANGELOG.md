fb37e4ce2e405ba7b8a9dd4c431870a53911635a
jrswab <jrswab@gmail.com>
2018-08-30 21:51:50 -0400

    Set node init script and init check script.
    The node init script (node/init.py) sends a custome json operation to the STEEM blockchain to broadcast that the node is live and ready to pin hashes sent to the Nebulus STEEM account.
    
    The init check script (node/networkCheck.py) looks over the past number of operations for any new node init custom json. If found, then adds a line to a file for the any future scripts that need to know the total number of nodes on the network. This may not be needed if we end up not needing the total numebr of nodes.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,10 +7,17 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [Unreleased]
 ### Added
 - Ability to pin hashes on home page without an account
+- Node directory to hold node scripts
+- Ability to send node live status with custom json
+- Ability for nodes to check custom json for node count
 
-### Deprecated
+### Removed
 - Creation of new user profile pages
 - Creation of new user feeds
+
+### Deprecated
+- RSS feeds
+- Profile pages
 
 ## [1.0.0](https://gitlab.com/jrswab/nebulus/commit/b585d3b4137c354e17e930dbb5bb766ebf3dbffc) - 2018-08-27
 ### Added
---
+++ node/networkCheck.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+from beem.blockchain import Blockchain
+from beem.block import Block
+
+# set chain to the needed blockchain params
+chain = Blockchain(steem_instance=None, mode='irreversible',
+        max_block_wait_repetition=None, data_refresh_time_seconds=900)
+
+def countNodes(name):
+    syntax = '+ ' + name
+    with open('config/nodeList', 'r') as count:
+        nodes = count.readlines()
+
+    if syntax in nodes:
+        print("Node already in list")
+    else:
+        with open('nodeList', 'a') as nodeList:
+            nodeList.write('\n+ ' + name)
+
+def getStartBlock():
+    try:
+        # Look for an int in the specified file
+        with open('config/startBlock', 'r') as startBlock:
+            sBlock = startBlock.read().replace('\n', '')
+    except FileNotFoundError:
+        # if no file; assume new node and start at first init block
+        sBlock = 25506548
+    return sBlock
+
+def setEndBlock():
+    # save current block on run for next starting block
+    eBlock = chain.get_current_block().block_num
+    return eBlock
+
+def setStartBlock(eBlock):
+    with open('config/startBlock', 'w') as newStart:
+        newStart.write(str(eBlock))
+
+# set block variables
+sBlock = int(getStartBlock())
+eBlock = int(setEndBlock())
+
+# TODO: start lookup chain at last stop point
+# write last block checked into a text file.
+for operation in chain.stream(opNames=['custom_json'],
+        raw_ops=False, start=sBlock, stop=eBlock):
+    # print out of proper running confirmation
+    print('Checking Block #'+ str(operation['block_num']))
+    # grab all operations with 'nebulus_node' between start and stop
+    if 'nebulus_node' in operation['id']:
+        # grab all init claims
+        if 'initialized' in operation['json']:
+            name = str(operation['required_posting_auths'])
+            # pass name for insertion into init list
+            countNodes(name[2:-2])
+        else:
+            print("No new init found in this Nebulus Json")
+
+
+setStartBlock(eBlock)
+
